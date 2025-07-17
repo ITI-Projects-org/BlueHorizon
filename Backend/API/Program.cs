af65b43420cfc9885cb16161ee143eae20660081
Muhammad El-Sabbagh <60693235+Muhmd-Sabagh@users.noreply.github.com>
2025-07-17 21:18:12 +0300

    connection string modified (database name changed) (#4)
    Co-authored-by: melsa <melsa@MUHMD-SABAGH-PC>

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -16,36 +16,37 @@ namespace API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            builder.Services.AddDbContext<VillageSystemDbContext>(options => 
-            options.UseLazyLoadingProxies()
-            .UseSqlServer(builder.Configuration.GetConnectionString("cs")));
+
+            builder.Services.AddDbContext<VillageSystemDbContext>(options => options
+                .UseLazyLoadingProxies()
+                .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
+
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
             {
-                options.Password.RequireUppercase= false;
+                options.Password.RequireUppercase = false;
                 options.Password.RequireDigit = false;
-                options.Password.RequireUppercase=false;
+                options.Password.RequireUppercase = false;
                 options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireLowercase= false;
-                options.Password.RequiredLength= 3;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredLength = 3;
             })
                 .AddEntityFrameworkStores<VillageSystemDbContext>()
                 .AddDefaultTokenProviders();
-            
+
             builder.Services.AddOpenApi();
             //builder.Services.AddAutoMapper(typeof(MappingConfig).Assembly);
             builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingConfig>());
+
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-
-
             // Configure the HTTP request pipeline.
-            builder.Services.AddAuthentication(op => op.DefaultAuthenticateScheme="myschema")
+            builder.Services.AddAuthentication(op => op.DefaultAuthenticateScheme = "myschema")
             .AddJwtBearer("myschema", option =>
             {
-                var key = "this is secrete key  for admin role base";
+                var key = "this is secrete key for admin role base";
                 var secreteKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key));
 
                 option.TokenValidationParameters = new TokenValidationParameters()
@@ -54,28 +55,28 @@ namespace API
                     ValidateIssuer = false,
                     IssuerSigningKey = secreteKey
                 };
-            }  
+            }
             );
 
             builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-        builder => builder
-            .WithOrigins("http://localhost:4200") // <-- your frontend URL
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials());
-});
+            {
+                options.AddPolicy("AllowFrontend",
+                    builder => builder
+                        .WithOrigins("http://localhost:4200") // <-- your frontend URL
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials());
+            });
 
             var app = builder.Build();
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
-            
+
             app.UseCors("AllowFrontend");
             app.UseHttpsRedirection();
-            app.UseAuthentication();    
+            app.UseAuthentication();
             app.UseAuthorization();
             app.MapControllers();
             app.Run();
