13ca9262f313d27c22e5cb89c06c1921c0968132
Mark Magdy <markmagdy822000@gmail.com>
2025-07-17 16:48:10 +0300

    verify owner feature done

--- Village_System/Migrations/20250717133455_v4.Designer.cs
+++ Village_System/Migrations/20250717133455_v4.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Village_System.Models;
 
@@ -11,9 +12,11 @@ using Village_System.Models;
 namespace Village_System.Migrations
 {
     [DbContext(typeof(VillageSystemDbContext))]
-    partial class VillageSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250717133455_v4")]
+    partial class v4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -876,7 +879,7 @@ namespace Village_System.Migrations
                     b.HasOne("Village_System.Models.Unit", "Unit")
                         .WithMany("UnitAmenities")
                         .HasForeignKey("UnitId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.Navigation("Amenity");
