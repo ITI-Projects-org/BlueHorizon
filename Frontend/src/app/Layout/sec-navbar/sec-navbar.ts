7a5c8f4c44c4ab223191e37c2930d1e1d7125ef8
yosra-ehab <ehabyousra842@gmail.com>
2025-07-25 20:47:58 +0300

    second navbar component

---
+++ Frontend/src/app/Layout/sec-navbar/sec-navbar.ts
@@ -0,0 +1,42 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-sec-navbar',
+  standalone: true,
+  templateUrl: './sec-navbar.html',
+  styleUrl: './sec-navbar.css',
+})
+export class SecNavbar implements AfterViewInit {
+  @ViewChild('mainNavbar') mainNavbar!: ElementRef;
+  @ViewChild('heroSection') heroSection!: ElementRef;
+
+  isScrolled = false;
+
+  ngAfterViewInit(): void {
+    this.adjustHeroPadding();
+    this.updateNavbarStyle();
+
+    window.addEventListener('scroll', () => {
+      this.isScrolled = window.scrollY > 50;
+      this.updateNavbarStyle();
+    });
+  }
+
+  private adjustHeroPadding() {
+    if (this.mainNavbar && this.heroSection) {
+      const navbarHeight = this.mainNavbar.nativeElement.offsetHeight;
+      this.heroSection.nativeElement.style.paddingTop = `${navbarHeight}px`;
+    }
+  }
+
+  private updateNavbarStyle() {
+    const navbar = this.mainNavbar?.nativeElement;
+    if (!navbar) return;
+
+    if (this.isScrolled) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  }
+}
