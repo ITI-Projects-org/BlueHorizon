84f2326a984808b2cc28d49bf15ea06463928b78
melsa <melsa@MUHMD-SABAGH-PC>
2025-07-26 04:29:50 +0300

    home page modified

--- Frontend/src/app/Pages/home/home.ts
+++ Frontend/src/app/Pages/home/home.ts
@@ -1,9 +1,15 @@
+// app/Pages/home/home.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
+  styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  // You can add properties and methods here if needed for dynamic content
+}
