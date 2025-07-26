e211d1801aee47c50bbbbf3cebf3bbb44150ecae
melsa <melsa@MUHMD-SABAGH-PC>
2025-07-26 04:16:19 +0300

    matrial package removed

--- Frontend/src/app/Components/unit-details/unit-details.ts
+++ Frontend/src/app/Components/unit-details/unit-details.ts
@@ -3,18 +3,15 @@ import { ActivatedRoute } from '@angular/router';
 import { Unit } from '../../Services/unit';
 import { IUnit } from '../../Models/iunit';
 import { CommonModule } from '@angular/common';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
-import { MatIconModule } from '@angular/material/icon';
-
 
 @Component({
   selector: 'app-unit-details',
   standalone: true,
-  imports: [CommonModule, MatProgressSpinnerModule, MatIconModule],
+  imports: [CommonModule],
   templateUrl: './unit-details.html',
-  styleUrl: './unit-details.css'
+  styleUrl: './unit-details.css',
 })
 export class UnitDetailsComponent implements OnInit {
   unit: IUnit | null = null;
@@ -30,15 +27,18 @@ export class UnitDetailsComponent implements OnInit {
       this.loading = false;
       return;
     }
-    this.unitService.GetUnitById(id).pipe(
-      catchError(err => {
-        this.error = 'Failed to load unit details.';
+    this.unitService
+      .GetUnitById(id)
+      .pipe(
+        catchError((err) => {
+          this.error = 'Failed to load unit details.';
+          this.loading = false;
+          return of(null);
+        })
+      )
+      .subscribe((unit) => {
+        this.unit = unit;
         this.loading = false;
-        return of(null);
-      })
-    ).subscribe((unit) => {
-      this.unit = unit;
-      this.loading = false;
-    });
+      });
   }
 }
--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -2,11 +2,19 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './Layout/footer/footer';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UnitDetailsComponent } from "./Components/unit-details/unit-details";
+import { UnitDetailsComponent } from './Components/unit-details/unit-details';
+import { Navbar } from './Layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Navbar, FormsModule, ReactiveFormsModule, UnitDetailsComponent],
+  imports: [
+    RouterOutlet,
+    Footer,
+    Navbar,
+    FormsModule,
+    ReactiveFormsModule,
+    UnitDetailsComponent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
